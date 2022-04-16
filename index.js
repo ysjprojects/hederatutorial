b02e27cbf22ae02d65c4421736c2d1b08dab0d9a
ysjprojects <85871692+ysjprojects@users.noreply.github.com>
2022-04-16 20:04:18 +0800

    Submit Your First Message

--- index.js
+++ index.js
@@ -1,71 +1,50 @@
-const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar, TransferTransaction } = require("@hashgraph/sdk");
 require("dotenv").config();
+const {
+    AccountId,
+    PrivateKey,
+    Client,
+    TopicCreateTransaction,
+    TopicMessageQuery,
+    TopicMessageSubmitTransaction,
+} = require("@hashgraph/sdk");
+
+// Grab the OPERATOR_ID and OPERATOR_KEY from the .env file
+const operatorId = AccountId.fromString(process.env.OPERATOR_ID);
+const operatorKey = PrivateKey.fromString(process.env.OPERATOR_KEY);
+
+// Build Hedera testnet and mirror node client
+const client = Client.forTestnet();
+
+// Set the operator account ID and operator private key
+client.setOperator(operatorId, operatorKey);
 
 async function main() {
-
-    //Grab your Hedera testnet account ID and private key from your .env file
-    const myAccountId = process.env.MY_ACCOUNT_ID;
-    const myPrivateKey = process.env.MY_PRIVATE_KEY;
-
-    // If we weren't able to grab it, we should throw a new error
-    if (myAccountId == null ||
-        myPrivateKey == null ) {
-        throw new Error("Environment variables myAccountId and myPrivateKey must be present");
-    }
-
-    // Create our connection to the Hedera network
-    // The Hedera JS SDK makes this really easy!
-    const client = Client.forTestnet();
-
-    client.setOperator(myAccountId, myPrivateKey);
-
-    //Create new keys
-    const newAccountPrivateKey = await PrivateKey.generateED25519(); 
-    const newAccountPublicKey = newAccountPrivateKey.publicKey;
-
-    //Create a new account with 1,000 tinybar starting balance
-    const newAccount = await new AccountCreateTransaction()
-    .setKey(newAccountPublicKey)
-    .setInitialBalance(Hbar.fromTinybars(1000))
-    .execute(client)
-
-    // Get the new account ID
-    const getReceipt = await newAccount.getReceipt(client);
-    const newAccountId = getReceipt.accountId;
-
-    //Log the account ID
-    console.log("The new account ID is: " +newAccountId);
-
-    //Verify the account balance
-    const accountBalance = await new AccountBalanceQuery()
-    .setAccountId(newAccountId)
-    .execute(client);
-
-    console.log("The new account balance is: " +accountBalance.hbars.toTinybars() +" tinybar.");
-
-    //Create the transfer transaction
-    const sendHbar = await new TransferTransaction()
-    .addHbarTransfer(myAccountId, Hbar.fromTinybars(-1000))
-    .addHbarTransfer(myAccountId, Hbar.fromTinybars(1000))
-    .execute(client)
-
-    //Verify the transaction reached consensus
-    const transactionReceipt = await sendHbar.getReceipt(client);
-    console.log("The transfer transaction from my account to the new account was: " + transactionReceipt.status.toString());
-
-    const queryCost = await new AccountBalanceQuery()
-    .setAccountId(newAccountId)
-    .getCost(client)
-
-    console.log("The cost of query is: " +queryCost);
-
-    //Check the new account's balance
-    const getNewBalance = await new AccountBalanceQuery()
-    .setAccountId(newAccountId)
-    .execute(client);
-
-    console.log("The account balance after the transfer is: " +getNewBalance.hbars.toTinybars() +" tinybar.")
-
-
+    //Create a new topic
+    let txResponse = await new TopicCreateTransaction().execute(client);
+    
+    //Grab the newly generated topic ID
+    let receipt = await txResponse.getReceipt(client);
+    let topicId = receipt.topicId;
+    console.log(`Your topic ID is: ${topicId}`);
+    
+    // Wait 5 seconds between consensus topic creation and subscription creation
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    
+    //Create the query
+    new TopicMessageQuery().setTopicId(topicId).subscribe(client, null, (message) => {
+        let messageAsString = Buffer.from(message.contents, "utf8").toString();
+        console.log(`${message.consensusTimestamp.toDate()} Received: ${messageAsString}`);
+    });
+    
+    // Send one message
+    let sendResponse = await new TopicMessageSubmitTransaction({
+        topicId: topicId,
+        message: "Hello, HCS!",
+    }).execute(client);
+    const getReceipt = await sendResponse.getReceipt(client);
+    
+    //Get the status of the transaction
+    const transactionStatus = getReceipt.status
+    console.log("The message transaction status" + transactionStatus)
 }
 main();
